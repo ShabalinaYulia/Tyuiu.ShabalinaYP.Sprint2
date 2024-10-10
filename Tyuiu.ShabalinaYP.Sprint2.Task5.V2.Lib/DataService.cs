f803f55ecd521d179cb3d265818862ee21080bf6
ShabalinaYulia <julia07.kurash@gmail.com>
2024-10-10 16:31:25 +0500

    Task5

--- Tyuiu.ShabalinaYP.Sprint2.Task5.V2.Lib/DataService.cs
+++ Tyuiu.ShabalinaYP.Sprint2.Task5.V2.Lib/DataService.cs
@@ -10,25 +10,25 @@ namespace Tyuiu.ShabalinaYP.Sprint2.Task5.V2.Lib
             {
                 case 1:
                 case 2:
-                    res = "Зима";
+                    res = "зима";
                     break;
                 case 3:
                 case 4:
                 case 5:
-                    res = "Весна";
+                    res = "весна";
                     break;
                 case 6:
                 case 7:
                 case 8:
-                    res = "Лето";
+                    res = "лето";
                     break;
                 case 9:
                 case 10:
                 case 11:
-                    res = "Осень";
+                    res = "осень";
                     break;
                 case 12:
-                    res = "Зима";
+                    res = "зима";
                     break;
                 default:
                     throw new ArgumentException($"Месяц должен быть от 1 до 12. Значение {value}");
