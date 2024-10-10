83ee35ce9f42f514d0cc07a4c4b32e5021aaec59
ShabalinaYulia <julia07.kurash@gmail.com>
2024-10-10 15:10:24 +0500

    task4

--- Tyuiu.ShabalinaYP.Sprint2.Task4.V15.Lib/DataService.cs
+++ Tyuiu.ShabalinaYP.Sprint2.Task4.V15.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.ShabalinaYP.Sprint2.Task4.V15.Lib
         public double Calculate(double x, double y)
         {
             double z = ((Math.Pow(x, 0.5) / 2) > (Math.Pow(y, 0.5) - 20)) ? Math.Pow((3 + 2 / (Math.Pow(y, 2))), x) : Math.Cos(x) + (12 /Math.Pow(x, 2));
-            return z;
+            return Math.Round(z, 3);
         }
     }
 }
