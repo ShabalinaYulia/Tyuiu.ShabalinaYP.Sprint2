156295e1c9d5c4130d421c002d5dea17f9a9da81
ShabalinaYulia <julia07.kurash@gmail.com>
2024-10-10 15:12:00 +0500

    Task4

--- Tyuiu.ShabalinaYP.Sprint2.Task4.V15/Program.cs
+++ Tyuiu.ShabalinaYP.Sprint2.Task4.V15/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Tyuiu.ShabalinaYP.Sprint2.Task4.V15
+﻿using Tyuiu.ShabalinaYP.Sprint2.Task4.V15.Lib;
+namespace Tyuiu.ShabalinaYP.Sprint2.Task4.V15
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите значение X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Значение: " + res);
+            Console.ReadKey();
         }
     }
 }
