f7a1eecaec71394693979e4ee907fc98a4a6d6c1
ShabalinaYulia <julia07.kurash@gmail.com>
2024-10-12 11:53:14 +0500

    task6

--- Tyuiu.ShabalinaYP.Sprint2.Task6.V14.Test/DataServiceTest.cs
+++ Tyuiu.ShabalinaYP.Sprint2.Task6.V14.Test/DataServiceTest.cs
@@ -1,11 +1,39 @@
+using Tyuiu.ShabalinaYP.Sprint2.Task6.V14.Lib;
+
 namespace Tyuiu.ShabalinaYP.Sprint2.Task6.V14.Test
+
 {
+
     [TestClass]
-    public class UnitTest1
+
+    public class DataServiceTest
+
     {
+
         [TestMethod]
-        public void TestMethod1()
+
+        public void ValidFindDayName()
+
         {
+
+            DataService ds = new DataService();
+
+            int k = 15;
+
+            int d = 7;
+
+            string res = ds.FindDayName(k, d);
+
+            string wait = "воскресенье";
+
+            Assert.IsTrue(res == wait);
+
+
+
+
+
         }
+
     }
+
 }
--- Tyuiu.ShabalinaYP.Sprint2.Task6.V14/Program.cs
+++ Tyuiu.ShabalinaYP.Sprint2.Task6.V14/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace Tyuiu.ShabalinaYP.Sprint2.Task6.V14
+﻿using Tyuiu.ShabalinaYP.Sprint2.Task6.V14.Lib;
+
+namespace Tyuiu.ShabalinaYP.Sprint2.Task6.V14
+
 {
+
     internal class Program
+
     {
+
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello, World!");
+
+            DataService ds = new DataService();
+            
+
+
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите номер дня:");
+
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите день с какого начался год");
+
+            int d = Convert.ToInt32(Console.ReadLine());
+
+            string res = ds.FindDayName(k, d);
+
+
+
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("День недели " + res);
+
+            Console.ReadKey();
+
+
+
         }
+
     }
+
 }
+
