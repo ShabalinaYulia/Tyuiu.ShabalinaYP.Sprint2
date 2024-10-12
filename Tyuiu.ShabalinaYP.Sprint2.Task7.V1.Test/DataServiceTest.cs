6c547c0acdf15bcc1c7a4905e6970f19ec6aa0d9
ShabalinaYulia <julia07.kurash@gmail.com>
2024-10-12 11:08:58 +0500

    task6

---
+++ Tyuiu.ShabalinaYP.Sprint2.Task7.V1.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.ShabalinaYP.Sprint2.Task7.V1.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
