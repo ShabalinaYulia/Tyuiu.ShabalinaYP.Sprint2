ca2b3dfbbdf2467f4907fd278efb7a0142456e06
ShabalinaYulia <julia07.kurash@gmail.com>
2024-10-14 16:26:03 +0500

    Task7

--- Tyuiu.ShabalinaYP.Sprint2.Task7.V1.Lib/DataService.cs
+++ Tyuiu.ShabalinaYP.Sprint2.Task7.V1.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.ShabalinaYP.Sprint2.Task7.V1.Lib
         public bool CheckDotInShadedArea(double x, double y)
         {
             bool res;
-            if ((Math.Pow(x, 2) + Math.Pow(y, 2) >= 1) && (y == x))
+            if (((Math.Pow(Math.Cos(x), 2) + Math.Pow(Math.Sin(y), 2)) == 1) && (y >= x) && (y >= -x)) 
             {
                 res = true;
             }
